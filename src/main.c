359213fa78169ef5dd3eb66ba70579514a717656
Eltaj Gafarli <eltacqeferli@gmail.com>
2026-06-18 23:45:54 +0400

    Task-22: add configurable initial bucket count

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <string.h>
+#include <errno.h>
 #include "../include/command.h"
 #include "../include/parser.h"
 #include "../include/shell.h"
@@ -10,12 +12,26 @@
 
 #define MAX_INPUT_SIZE 1024
 
+typedef enum {
+    CLI_PARSE_OK,
+    CLI_PARSE_INVALID
+} CliParseResult;
+
+typedef struct {
+    const char *file_path;
+    size_t bucket_count;
+    bool bucket_count_provided;
+} CliConfig;
+
 static volatile sig_atomic_t keep_running = 1;
 
 void handle_sigint(int sig) {
     keep_running = 0;
 }
 
+static CliParseResult parse_cli_args(int argc, char *argv[], CliConfig *config);
+
+static void init_cli_config(CliConfig *config);
 
 int main(int argc, char *argv[]) {
 
@@ -26,22 +42,33 @@ int main(int argc, char *argv[]) {
 
     char input[MAX_INPUT_SIZE];
 
-    const char *file_path = "../data/pagedb.data";
+    CliConfig cli_config;
 
-    if (argc == 2) {
-        file_path = argv[1];
-    } else if (argc > 2) {
-        printf("Usage: PageDB [database_file]\n");
-        return 1;
+    init_cli_config(&cli_config);
+
+
+    CliParseResult parse_cli_result = parse_cli_args(argc, argv, &cli_config);
+
+    if (parse_cli_result == CLI_PARSE_INVALID) {
+        printf("Usage: PageDB [database_file] [--bucket-count N]\n");
+        return EXIT_FAILURE;
     }
 
     KvStore store;
-    bool initialized = kv_store_init(&store);
+    bool initialized;
+
+    if (cli_config.bucket_count_provided) {
+        initialized = kv_store_init_with_bucket_count(&store, cli_config.bucket_count);
+    } else {
+        initialized = kv_store_init(&store);
+    }
 
     if (!initialized) {
         printf("Error: Failed to initialize KV store.\n");
         return EXIT_FAILURE;
     }
+
+    const char *file_path = cli_config.file_path;
 
     load_storage_log(&store, file_path);
 
@@ -84,4 +111,54 @@ int main(int argc, char *argv[]) {
     kv_store_destroy(&store);
 
     return EXIT_SUCCESS;
+}
+
+static CliParseResult parse_cli_args(int argc, char *argv[], CliConfig *config) {
+    const char *bucket_count_option = "--bucket-count";
+    bool file_path_provided = false;
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], bucket_count_option) == 0) {
+            if (config->bucket_count_provided) {
+                return CLI_PARSE_INVALID;
+            }
+
+            if (i + 1 >= argc) {
+                return CLI_PARSE_INVALID;
+            }
+
+            char *endptr = NULL;
+            errno = 0;
+
+            long value = strtol(argv[i + 1], &endptr, 10);
+
+            if (errno == ERANGE || endptr == argv[i + 1] || *endptr != '\0') {
+                return CLI_PARSE_INVALID;
+            }
+
+            if (value <= 0) {
+                return CLI_PARSE_INVALID;
+            }
+
+            config->bucket_count = (size_t)value;
+            config->bucket_count_provided = true;
+
+            i++;
+        } else {
+            if (file_path_provided) {
+                return CLI_PARSE_INVALID;
+            }
+
+            config->file_path = argv[i];
+            file_path_provided = true;
+        }
+    }
+
+    return CLI_PARSE_OK;
+}
+
+static void init_cli_config(CliConfig *config) {
+    config->file_path = "../data/pagedb.data";
+    config->bucket_count = DEFAULT_BUCKET_COUNT;
+    config->bucket_count_provided = false;
 }
